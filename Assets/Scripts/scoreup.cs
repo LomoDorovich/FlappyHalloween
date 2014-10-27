12207aeaebfa61453e53e369c15407bc68256f3a
CarmenGraus <carmengraus95@gmail.com>
2014-10-27 17:26:03 +0100

    BetaFinal

--- Assets/Scripts/scoreup.cs
+++ Assets/Scripts/scoreup.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 public class scoreup : MonoBehaviour {
 
 	private Text texto;
+	private Text textoFinal;
 
 	void Start (){
-				texto = GameObject.Find ("Text").GetComponent<Text> ();
-		}
+		texto = GameObject.Find ("TextoNivel").GetComponent<Text> ();
+		textoFinal = GameObject.Find ("TextoFinal").GetComponent<Text> ();
+	}
 	void OnTriggerEnter2D(Collider2D col){
 		GameControl.score = GameControl.score + 1;
 		//Debug.Log (GameControl.score);
 
 		texto.text = "Level " + GameControl.score.ToString ();
-	
+		textoFinal.text = texto.text;
 	}
 }
